856ca0fb08e3ee47ef456099a17ba16134866785
zivversano <ziv2405@gmail.com>
2025-04-13 21:17:03 +0000

    "marged branches"

--- Dev/main.py
+++ Dev/main.py
@@ -5,3 +5,20 @@ logger= logging.getLogger(__name__)
 if __name__ == '__main__':
     #TODO: smart bot 
     pass
+
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/get")
+def get_bot_response():
+    query = request.args.get('msg')
+    response = "this is an answer"
+    return response
